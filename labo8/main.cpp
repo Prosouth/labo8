8dbbc1bbe29ef13a0de9350362d888f2e76b0009
samhack <samhack@devhack>
2016-12-12 10:17:05 +0100

    oklm

---
+++ labo8/main.cpp
@@ -0,0 +1,28 @@
+
+/* ---------------------------
+ Laboratoire : 
+ Fichier :     main.cpp
+ Auteur(s) : Samuel Yao  
+ Date      : 12 December 2016, 10:16
+ 
+ But :          
+ 
+ Remarque(s) :
+ 
+ Compilateur :gcc g++ 5.4.0
+ --------------------------- */
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    cout << "test" << endl;
+
+    return EXIT_SUCCESS;
+}
+
