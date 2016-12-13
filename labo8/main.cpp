f0020987f284d0bdf2399cb2714d8f3b3d8beadc
Sebastien Saez <monsieursaez@gmail.com>
2016-12-13 11:11:40 +0100

    first draft. Added afficher function + initial array

--- labo8/main.cpp
+++ labo8/main.cpp
@@ -16,13 +16,66 @@
 
 using namespace std;
 
-/*
- * 
- */
-int main(int argc, char** argv) {
-    
-    cout << "test" << endl;
 
-    return EXIT_SUCCESS;
+// Constantes 
+const int NB_COLONNES = 7,
+          NB_LIGNES = 7;
+
+bool mouvementValide(string deplacement);
+void retraitPion(int indice_largeur, int indice_hauteur);
+void aide();
+void initialisationJeu(int tableau[][NB_LIGNES]);
+void afficher(int surfaceJeu[7][7]);
+
+
+int main() 
+{
+   int surfaceJeu[NB_COLONNES][NB_LIGNES] = {{0,0,2,2,2,0,0},
+                                             {0,0,2,2,2,0,0},
+                                             {2,2,2,2,2,2,2},
+                                             {2,2,2,1,2,2,2},
+                                             {2,2,2,2,2,2,2},
+                                             {0,0,2,2,2,0,0},
+                                             {0,0,2,2,2,0,0}};
+
+   afficher(surfaceJeu);
+   
+   
+   return EXIT_SUCCESS;
 }
 
+
+bool mouvementValide(string deplacement)
+{
+   
+}
+
+
+void retraitPion(int indice_largeur, int indice_hauteur)
+{
+   
+}
+
+
+void aide()
+{
+   
+}
+
+
+void initialisationJeu(int tableau[][NB_LIGNES])
+{
+   
+
+
+}
+
+void afficher(int surfaceJeu[][7])
+{
+   for (int i = 0; i < NB_COLONNES; i++) {
+      for (int j = 0; j < NB_LIGNES; j++) {
+         cout << surfaceJeu[i][j] << " ";
+      }
+      cout << endl;
+   }
+}
